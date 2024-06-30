835a13e81744ad61c41c159b8170b7d33089f906
Daniel galvan <danielricardogt@gmail.com>
2024-06-29 20:31:25 -0500

    feature/(LRNG-001): :construction: adding image and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import '../app/ui/global.css';
+import {montserrat} from '../app/ui/fonts'
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className= {`${montserrat.className} antialiased`}> 
+        <h1>Este titulo</h1>
+  
+        {children}
+       
+        
+        </body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+// Esto es una biblioteca interna denext js para importar cualquier fuente de gogoles fonts
+import {Montserrat, Lusitana} from "next/font/google"
+
+
+export const montserrat = Montserrat ({subsets:['latin']})
+export const lusitana = Lusitana ({weight: ['400','700'] ,subsets:['latin']})
+ 
